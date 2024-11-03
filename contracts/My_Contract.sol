2c5c29e4a2ccdcb58aadbdf18ade8e33457229eb
shivaram19 <shivaramiitmb085@gmail.com>
2024-11-03 19:11:12 +0530

    inheritance , factories

--- contracts/My_Contract.sol
+++ contracts/My_Contract.sol
@@ -2,6 +2,39 @@
 
 pragma solidity ^0.8.0;
 
-contract MyContract{
-    
+contract Ownable {
+    address owner;
+    modifier onlyOwner{
+        require(msg.sender == owner,"must be owner to acess this function");
+        _;
+    }
+    constructor() {
+        owner = msg.sender;
+    }
+}
+
+contract SecretVault{
+    string secret;
+    constructor(string memory _secret)  {
+        secret = _secret;
+    }
+    function getSecret() public view returns (string memory){
+        return secret;
+    }
+}
+// fatcory is a smart contract that creates other smart contract 
+contract MyContract is Ownable {
+    address secretVault;
+    //  inheritance 
+    // 
+    // factories
+    // interaction
+    constructor(string memory _secret)  {
+        SecretVault _secretVault = new SecretVault(_secret);
+        secretVault = address(_secretVault);
+        super;
+    }
+    function getSecret() public view onlyOwner returns (string memory){
+        return SecretVault(secretVault).getSecret();
+    }
 }
